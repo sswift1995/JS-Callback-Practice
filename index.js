5caf2ad238b1136bbb4b1fe822207d46ab769d1f
Syrinthia Swift <116780275+sswift1995@users.noreply.github.com>
2023-01-26 21:42:38 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
+
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+let direction = null;
+let x = 100;
+let y = 250;
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
+
+
+
+
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
